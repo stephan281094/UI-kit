c3594cd83ab5c7e176fc94a41772b26092c18065
Stephan <stephan281094@gmail.com>
2014-10-23 23:24:28 +0200

    Removed debug.js, gulpfile concat added

--- gulpfile.js
+++ gulpfile.js
@@ -5,16 +5,21 @@ var sass         = require('gulp-ruby-sass');
 var autoPrefix   = require('gulp-autoprefixer');
 var open         = require('gulp-open');
 var bower        = require('gulp-bower');
+var concat       = require('gulp-concat');
 var runSequence  = require('run-sequence');
 
 var paths = {
     javascripts: 'client/javascripts/*.coffee',
     stylesheets: 'client/stylesheets/*.sass',
-    indexFile: './index.html'
+    indexFile: './index.html',
+    styles: {
+        animatecss: 'libs/animate.css/animate.min.css',
+        normalizecss: 'libs/normalize.css/normalize.css'
+    }
 }
 
 gulp.task('bower', function(){
-    return bower();
+    return bower('./libs');
 });
 
 gulp.task('javascripts', function() {
@@ -25,15 +30,17 @@ gulp.task('javascripts', function() {
 
 gulp.task('stylesheets', function(){
     return gulp.src(paths.stylesheets)
-        .pipe(sass({ style: 'compressed' }))
+        .pipe(sass({ style: 'compact' }))
         .pipe(autoPrefix('last 15 versions'))
+        .pipe(concat('all.min.css'))
         .pipe(gulp.dest('build/stylesheets/'));
 });
 
-
 gulp.task('watch', function(){
     gulp.watch(paths.javascripts, ['javascripts']);
-    gulp.watch(paths.stylesheets, ['stylesheets']);
+    gulp.watch(paths.stylesheets, function(){
+        runSequence('stylesheets');
+    });
 });
 
 gulp.task('open', function(){
@@ -43,5 +50,5 @@ gulp.task('open', function(){
 
 
 gulp.task('default', function(){
-    runSequence(['bower', 'javascripts', 'stylesheets', 'watch'], 'open');
+    runSequence('bower', ['javascripts', 'stylesheets'], 'watch', 'open');
 });
