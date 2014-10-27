2b9770f8e2e99ae9260fc2ff4e3e27953454a38f
Stephan <stephan281094@gmail.com>
2014-10-27 01:55:46 +0100

    Added gulp-webserver and gulp-concat

--- gulpfile.js
+++ gulpfile.js
@@ -1,54 +1,79 @@
-var gulp         = require('gulp');
-var gutil        = require('gulp-util');
-var coffee       = require('gulp-coffee');
-var sass         = require('gulp-ruby-sass');
-var autoPrefix   = require('gulp-autoprefixer');
-var open         = require('gulp-open');
-var bower        = require('gulp-bower');
-var concat       = require('gulp-concat');
-var runSequence  = require('run-sequence');
+var gulp        = require('gulp');
+var gutil       = require('gulp-util');
+var bower       = require('gulp-bower');
+var sass        = require('gulp-ruby-sass');
+var coffee      = require('gulp-coffee');
+var autoprefix  = require('gulp-autoprefixer');
+var concat      = require('gulp-concat');
+var webserver   = require('gulp-webserver');
+var runSequence = require('run-sequence');
 
 var paths = {
-    javascripts: 'client/javascripts/*.coffee',
-    stylesheets: 'client/stylesheets/*.sass',
-    indexFile: './index.html',
-    styles: {
-        animatecss: 'libs/animate.css/animate.min.css',
-        normalizecss: 'libs/normalize.css/normalize.css'
+    sass: './client/stylesheets/sass/*.sass',
+    css: './client/stylesheets/css/*.css',
+    coffee: './client/javascripts/coffee/*.coffee',
+    js: './client/javascripts/js/*.js',
+    libs: {
+        css: {
+            animate: './client/libs/animate.css/animate.min.css',
+            normalize: './client/libs/normalize.css/normalize.css'
+        },
+        js: {
+            // Add path of new js library
+        }
     }
 }
 
+var webserver_config = {
+    open: true
+}
+
+
 gulp.task('bower', function(){
-    return bower('./libs');
+    return bower('./client/libs');
 });
 
-gulp.task('javascripts', function() {
-    return gulp.src(paths.javascripts)
-        .pipe(coffee({bare: true}).on('error', gutil.log))
-        .pipe(gulp.dest('build/javascripts/'));
-});
-
-gulp.task('stylesheets', function(){
-    return gulp.src(paths.stylesheets)
+gulp.task('sass', function(){
+    return gulp.src(paths.sass)
         .pipe(sass({ style: 'compact' }))
-        .pipe(autoPrefix('last 15 versions'))
+        .pipe(autoprefix('last 15 versions'))
+        .pipe(gulp.dest('client/stylesheets/css/'));
+});
+
+gulp.task('coffee', function() {
+    return gulp.src(paths.coffee)
+        .pipe(coffee({bare: true}).on('error', gutil.log))
+        .pipe(gulp.dest('client/javascripts/js/'));
+});
+
+// Make sure to always put libraries first
+gulp.task('concat_css', function(){
+    return gulp.src([paths.libs.css.animate, paths.libs.css.normalize, paths.css])
         .pipe(concat('all.min.css'))
-        .pipe(gulp.dest('build/stylesheets/'));
+        .pipe(gulp.dest('./build/css/'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch(paths.javascripts, ['javascripts']);
-    gulp.watch(paths.stylesheets, function(){
-        runSequence('stylesheets');
-    });
+// Make sure to always put libraries first
+gulp.task('concat_js', function(){
+    return gulp.src(paths.js)
+        .pipe(concat('all.min.js'))
+        .pipe(gulp.dest('./build/js/'));
 });
 
-gulp.task('open', function(){
-    gulp.src(paths.indexFile)
-        .pipe(open("<%file.path%>"));
+gulp.task('webserver', function() {
+    gulp.src('./')
+        .pipe(webserver(webserver_config));
 });
 
 
 gulp.task('default', function(){
-    runSequence('bower', ['javascripts', 'stylesheets'], 'watch', 'open');
+    runSequence(
+        'bower',
+        ['sass', 'coffee'],
+        ['concat_css', 'concat_js'],
+        'webserver'
+    );
+
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.coffee, ['coffee']);
 });
